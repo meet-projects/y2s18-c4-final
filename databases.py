b22128960cba7900961353f63bfe7a6ac398b378
Shmuel19-meet <shmuel19-meet@meet.mit.edu>
2018-08-12 23:51:07 +0300

    post database changes

--- databases.py
+++ databases.py
@@ -1,6 +1,6 @@
 # Database related imports
 # Make sure to import your tables!
-from model import Base, User
+from model import Base, User, Post
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -9,31 +9,50 @@ from sqlalchemy.orm import sessionmaker
 # Make sure you have the same name for the database in the app.py file!
 engine = create_engine('sqlite:///project.db')
 Base.metadata.create_all(engine)
-DBSession = sessionmaker(bind=engine)
+DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
 # Your database functions are located under here (querying, adding items, etc.)
 
 # Example of adding a student:
-def add_user(name, password, points, picture):
+def add_user(name, password, points):
+	
 	print("Added a user!")
-	user = User(name = name, password = password, points = points, picture = picture)
+	user = User(name = name, password = password, points = points)
 	session.add(user)
 	session.commit()
 
-def query_all():
-	students = session.query(User).all()
-	return students
+def add_post(title, description):
+
+	print("Added a post!")
+	post = Post(title = title, description = description)
+	session.add(post)
+	session.commit()
+
+def query_all_users():
+
+	return session.query(User).all()
+
+def query_all_posts():
+
+	return session.query(Post).all()
 
 def query_by_name(name):
+
 	return session.query(User).filter_by(name = name).all()
 
-def query_by_status(status):
-	return session.query(User).filter_by(fraud = status).all()
+def query_by_title(title):
 
-def delete_all():
+	return session.query(Post).filter_by(title = title).all()
+
+def delete_all_users():
 	
 	session.query(User).delete()
+	session.commit()
+
+def delete_all_posts():
+	
+	session.query(Posts).delete()
 	session.commit()
 
 def delete_duplicates():
