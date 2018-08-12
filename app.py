131a102f8b1199309e3b10b8f07dcfd239e530d7
aboud20-meet <aboud20@meet.mit.edu>
2018-08-12 17:44:00 +0300

    sign up and sign flask

--- app.py
+++ app.py
@@ -11,21 +11,21 @@ app = Flask(__name__)
 # App routing code here
 @app.route('/lognin' , methods=['GET','POST'])
 def loginpage():
-	if request.method == 'POST':
-		user = databases.query_by_name(request.form["name"])
-	
+    if request.method == 'POST':
+        user = databases.query_by_name(request.form["name"])
+    
 
-		if user is not None:
-			if user.password == request.form["password"]:
-				return redirect(url_for("profilepage"))
+        if user is not None:
+            if user.password == request.form["password"]:
+                return redirect(url_for("profilepage"))
 
-			else:
-				error = 'password does not match'
-				return render_template('home.html', error=error)
+            else:
+                error = 'password does not match'
+                return render_template('home.html', error=error)
         else:
 
-		    error = 'username does not exist'
-		    return render_template('home.html', error=error)
+            error = 'username does not exist'
+            return render_template('home.html', error=error)
 
     else:
 
@@ -36,24 +36,24 @@ def loginpage():
 
 @app.route('/profile')
 def profilepage():
-	pass
+    pass
 
 @app.route('/signup', methods['GET' , 'POST'])
 def signuppage():
-	if request.method == 'GET':
-		return render_template(signup.html)
-	else:
-		name= request.form['thename']
-		password = request.form['thepassword']
+    if request.method == 'GET':
+        return render_template(signup.html)
+    else:
+        name= request.form['thename']
+        password = request.form['thepassword']
 
 
-		
-		databases.add_user(name , password, 0 )
-		return render_template(profile.html)
+        
+        databases.add_user(name , password, 0 )
+        return render_template(loginpage)
 
-		
+        
 
 
-	
+    
 if __name__ == "__main__":
     app.run(debug=True)
